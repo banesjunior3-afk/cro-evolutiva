b72c8bddb2a498d6eca4a83d13fff1000d09ef97
banesjunior3-afk <banesjunior3@gmail.com>
2026-03-11 14:12:29 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static("."));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log("Servidor rodando");
+  console.log("Servidor rodando na porta " + PORT);
 });
